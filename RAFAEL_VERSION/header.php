f7827cec30f73aa8371df5ceab8c6dda1268daf7
Sara Lana <saralgc@gmail.com>
2021-02-06 18:35:20 -0300

    correcoes gui

---
+++ RAFAEL_VERSION/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" href="css/main.css" >
+
+    <title>Nossa Horta</title>
+  </head>
+  <body>
+
+
+    <!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top opa-15">
+
+  <a class="navbar-brand" href="#">
+      <img id="logo-topo" src="img/csanossahorta-branco.png" alt=""> 
+  </a>
+
+  <div class="container">
+   
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              SOBRE
+                <span class="sr-only">(current)</span>
+              </a>
+
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="HHTP://google.com">Carta de Princípios</a>
+
+              </div>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            PRODUTORES
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">CESTAS SEMANAIS</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">RECEITAS</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#">CONTATOS</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ RAFAEL_VERSION/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<?php include('header.php') ?>
+</head>
+<body>
+	<?php include('header.php') ?>
+	<div class="content">
+		<!-- Seu conteudo -->
+	</div>
+	<?php include('footer.php') ?>
+</body>
+</html>
